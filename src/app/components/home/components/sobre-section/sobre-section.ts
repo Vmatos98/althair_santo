b984de881aa0fa0ee1c60215791307f9ed2f8da0
antoniovitormatos <antoniovitormatos@hotmail.com>
2025-12-04 00:14:12 -0300

    Ajustes primeira versão

--- src/app/components/home/components/sobre-section/sobre-section.ts
+++ src/app/components/home/components/sobre-section/sobre-section.ts
@@ -13,8 +13,9 @@ export class SobreSection {
 
   sobreData = {
     title: 'A Alma do Design Autoral',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    description2: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    description: 'Quando se olha para o trabalho de Althair Santo, a força criativa sergipana aparece de imediato. Formado em Design de Moda e em Português/Francês pela UFS, ele enxerga a moda como linguagem e obra de arte.  Um caminho para contar histórias e valorizar o território que o formou.',
+    description2: 'Sua produção nasce de uma rede colaborativa de artesãos de Sergipe, que fortalece cada etapa e imprime sentido às peças. Ele valoriza as múltiplas vidas de uma roupa: das mãos que bordam à presença de quem veste, carregando cultura e identidade.',
+    description3: 'Com trajetória que cruzou marcas como Guess U.S.E., TV Globo, SPFW, Fashion Rio, Innovamoda e o Prêt-à-Porter de Paris, Althair também foi consultor da Rede Sergipe Design com o IED de Milão, estudou moulage em Paris, orientou coleção premiada no DFB Festival, coordenou o curso de estilista do SENAC Sergipe e lecionou moda na Áustria. Hoje, como embaixador do Fashion Revolution Brasil em São Paulo e integrante do Brasil Eco Fashion Week, segue criando uma moda que une arte, território e pensamento contemporâneo.',
     features: [
       {
         icon: 'fas fa-award',
