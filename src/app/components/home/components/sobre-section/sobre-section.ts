2678d8c4549df7e2c4f3e60d93851e2f1c35d911
Vmatos98 <antoniovitomatos@gmail.com>
2026-07-03 14:24:07 -0300

    refactor: update institutional description to include full name of SENAI CETIQT

--- src/app/components/home/components/sobre-section/sobre-section.ts
+++ src/app/components/home/components/sobre-section/sobre-section.ts
@@ -13,7 +13,7 @@ export class SobreSection {
 
   sobreData = {
     title: 'A Alma do Design Autoral',
-    description: 'Quando se olha para o trabalho de Althair Santo, a força criativa sergipana aparece de imediato. Formado em Design de moda pela faculdade do Centro de Tecnologia da Indústria Quite Textil do Cetiqt do Rio de Janeiro e em Português/Francês pela UFS, ele enxerga a moda como linguagem e obra de arte.  Um caminho para contar histórias e valorizar o território que o formou.',
+    description: 'Quando se olha para o trabalho de Althair Santo, a força criativa sergipana aparece de imediato. Formado em Design de moda pela faculdade do SENAI CETIQT (Centro de Tecnologia da Indústria Química e Têxtil) do Rio de Janeiro e em Português/Francês pela UFS, ele enxerga a moda como linguagem e obra de arte.  Um caminho para contar histórias e valorizar o território que o formou.',
     description2: 'Sua produção nasce de uma rede colaborativa de artesãos de Sergipe, que fortalece cada etapa e imprime sentido às peças. Ele valoriza as múltiplas vidas de uma roupa: das mãos que bordam à presença de quem veste, carregando cultura e identidade.',
     description3: 'Com trajetória que cruzou marcas como Guess U.S.E., TV Globo, SPFW, Fashion Rio, Innovamoda e o Prêt-à-Porter de Paris, Althair também foi consultor da Rede Sergipe Design com o IED de Milão, estudou moulage em Paris, orientou coleção premiada no DFB Festival, coordenou o curso de estilista do SENAC Sergipe e lecionou moda na Áustria. Hoje, como embaixador do Fashion Revolution Brasil em São Paulo e integrante do Brasil Eco Fashion Week, segue criando uma moda que une arte, território e pensamento contemporâneo.',
     features: [
