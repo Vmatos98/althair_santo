d6f321bc6639bd47e48cb0a83f1cb14227d677e5
Vmatos98 <antoniovitomatos@gmail.com>
2026-08-28 21:05:44 -0300

    refactor: update collections section to display three items in a grid and remove unused data entries

--- src/app/components/colecoes/colecoes.ts
+++ src/app/components/colecoes/colecoes.ts
@@ -31,20 +31,6 @@ export class Colecoes {
       description: 'Brasil Eco Fashion é uma plataforma que promove as boas práticas do mercado e de marcas inseridas na indústria da moda que vem se desenvolvendo com foco nos atributos de sustentabilidade e artesanato de luxo.',
       image: 'https://i.postimg.cc/VL0KfpmW/altair-santo-befw23-011.jpg',
       imagePosition: 'left'
-    },
-    {
-      id: 'milan-fashion-week',
-      title: 'MILAN FASHION WEEK',
-      description: 'Inspiração italiana em cada linha, combinando a elegância clássica de Milão com inovações em design e técnica artesanal.',
-      image: 'https://res.cloudinary.com/xbovcgmp/image/upload/WhatsApp_Image_2026-08-26_at_17.35.14.jpg',
-      imagePosition: 'right'
-    },
-    {
-      id: 'new-york-fashion-week',
-      title: 'NEW YORK FASHION WEEK',
-      description: 'A energia pulsante de Nova York traduzida em moda, onde cada peça conta uma história de inovação e sofisticação urbana.',
-      image: 'https://res.cloudinary.com/xbovcgmp/image/upload/WhatsApp_Image_2026-08-26_at_17.35.02.jpg',
-      imagePosition: 'left'
     }
   ];
 
